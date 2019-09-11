e58b4faf36374e0666965cabd76c66b89c9ef9c7
Martin Metodiev <metodiev.martin@gmail.com>
2019-09-11 19:28:33 +0300

    Extend viewport meta tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -204,6 +204,8 @@ export default {
       '@nuxtjs/pwa',
       {
         meta: {
+          viewport:
+            'width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no',
           mobileAppIOS: true,
           favicon: true,
           name: title,
