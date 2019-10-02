59de05f06ae07e95559f0c42d6f55389ec226e82
Martin Metodiev <metodiev.martin@gmail.com>
2019-10-02 17:48:29 +0300

    Update "mm-atomic-pack" to v1.10.0

--- components/organisms/Footer/footer.js
+++ components/organisms/Footer/footer.js
@@ -1,9 +1,18 @@
 // ===================================================|
 // Organisms: FOOTER
 
+import { mapState } from 'vuex';
+
+// Components
 import Logo from '@/components/atoms/Logo';
-import Copyright from '@/components/atoms/Copyright';
+import Copyright from '@/node_modules/mm-atomic-pack/components/atoms/Copyright';
 
 export default {
-  components: { Logo, Copyright }
+  components: { Logo, Copyright },
+
+  computed: mapState({
+    domain: (state) => state.contentful.data.copyright.fields.domain,
+    foundationYear: (state) =>
+      state.contentful.data.copyright.fields.foundationYear
+  })
 };
